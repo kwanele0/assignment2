081ae9c742c66ad29738a0c9d27d9b3f83de050f
kwanele07 <84250893+kwanele07@users.noreply.github.com>
2021-05-16 10:24:59 +0200

    Add files via upload

---
+++ Kwanele.java
@@ -0,0 +1,12 @@
+/*Kwanele Sobekwa
+  217299032
+  Java Assignment 2
+*/
+
+import javax.swing.*;
+public class Kwanele {
+    public static void main(String[] args) {
+    JOptionPane.showMessageDialog(null, "Programming is fun",null, JOptionPane.INFORMATION_MESSAGE);
+     System.exit(0);
+    } 
+}
